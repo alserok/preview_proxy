d57bd72260b3c191989621e759a55369d30caa8a
Alex Rokachev <rokaleser@gmail.com>
2024-12-11 12:46:29 +0300

    added initial redis setup

--- server/internal/cache/cache.go
+++ server/internal/cache/cache.go
@@ -3,10 +3,11 @@ package cache
 import (
 	"context"
 	"github.com/alserok/preview_proxy/server/internal/cache/redis"
+	r "github.com/go-redis/redis/v8"
 )
 
 type Cache interface {
-	Set(ctx context.Context, key string, value []byte) error
+	Set(ctx context.Context, key string, value any) error
 	Get(ctx context.Context, key string, target any) error
 
 	Close() error
@@ -16,10 +17,10 @@ const (
 	Redis = iota
 )
 
-func New(t uint) Cache {
+func New(t uint, cl ...any) Cache {
 	switch t {
 	case Redis:
-		return redis.NewCache()
+		return redis.NewCache(cl[0].(*r.Client))
 	default:
 		panic("invalid cache type")
 	}
--- server/internal/cache/redis/setup.go
+++ server/internal/cache/redis/setup.go
@@ -1,5 +1,20 @@
 package redis
 
-func MustConnect(addr string) {
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+)
 
+func MustConnect(addr string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+
+	if err := client.Ping(context.Background()).Err(); err != nil {
+		panic("failed to ping: " + err.Error())
+	}
+
+	return client
 }
