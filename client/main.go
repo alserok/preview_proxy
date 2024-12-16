848bc70b3adfb0a268257c5e680a71ac2c2d0787
Alex Rokachev <rokaleser@gmail.com>
2024-12-16 14:16:57 +0300

    updated ignore file

--- client/main.go
+++ client/main.go
@@ -10,24 +10,33 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"os"
-	"os/signal"
 	"path"
 	"strings"
 	"sync"
-	"syscall"
 )
 
 const (
-	async = iota + 1
+	async mode = iota + 1
 	localAsync
 )
 
-func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
+type mode uint
 
+func (m mode) String() string {
+	switch m {
+	case async:
+		return "server async"
+	case localAsync:
+		return "client async"
+	default:
+		return "sync"
+	}
+}
+
+func main() {
 	s := flag.Int("async", 0, "")
 	flag.Parse()
+	m := mode(uint(*s))
 
 	cc, err := grpc.NewClient(os.Getenv("SERVER_ADDR"), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -37,109 +46,127 @@ func main() {
 	client := proto.NewPreviewProxyClient(cc)
 
 	if _, err = os.Stat(filesDir); err != nil {
-		if err = os.Mkdir(filesDir, 755); os.IsNotExist(err) {
+		if err = os.Mkdir(filesDir, 0777); os.IsNotExist(err) {
 			panic("failed to init files folder: " + err.Error())
 		}
 	}
 
-	fmt.Println("ready")
+	fmt.Printf("ready\nmode: %s\n\n", m.String())
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		select {
-		case <-ctx.Done():
+		val := scanner.Text()
+		switch val {
+		case "":
+			continue
+		case "q":
 			return
 		default:
+			videoURLs := strings.Split(val, " ")
+
+			switch m {
+			case async:
+				serverAsync(client, videoURLs)
+			case localAsync:
+				clientAsync(client, videoURLs)
+			default:
+				defaultSync(client, videoURLs)
+			}
+		}
+	}
+}
+
+func serverAsync(client proto.PreviewProxyClient, videoURLs []string) {
+	res, err := client.GetThumbnails(context.Background(), &proto.GetThumbnailReq{
+		VideoUrls: videoURLs,
+		Async:     true,
+	})
+	if err != nil {
+		fmt.Printf("failed to get thumbnails: %s\n", err.Error())
+		return
+	}
+	for _, data := range res.Videos {
+		path, err := saveTo(context.Background(), strings.Split(data.VideoUrl, "?v=")[1], data.Thumbnail)
+		if err != nil {
+			res.Failed++
+			fmt.Printf("failed to save thumbnail: %s\n", err.Error())
+			continue
 		}
 
-		val := scanner.Text()
-		videoURLs := strings.Split(val, " ")
+		fmt.Printf("video url: %s\tthumbnail path: %s\n", data.VideoUrl, path)
+	}
 
-		switch *s {
-		case async:
+	fmt.Printf("successfully:%d\tfailed:%d\n", res.Total-res.Failed, res.Failed)
+	fmt.Println()
+}
+
+func clientAsync(client proto.PreviewProxyClient, videoURLs []string) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(videoURLs))
+	chData := make(chan *proto.GetThumbnailRes, len(videoURLs))
+
+	for _, url := range videoURLs {
+		go func() {
+			defer wg.Done()
+
 			res, err := client.GetThumbnails(context.Background(), &proto.GetThumbnailReq{
-				VideoUrls: videoURLs,
-				Async:     true,
-			})
-			if err != nil {
-				fmt.Printf("failed to get thumbnails: %s\n", err.Error())
-				continue
-			}
-
-			for _, data := range res.Videos {
-				path, err := saveTo(context.Background(), strings.Split(data.VideoUrl, "?v=")[1], data.Thumbnail)
-				if err != nil {
-					res.Failed++
-					fmt.Printf("failed to save thumbnail: %s\n", err.Error())
-					continue
-				}
-
-				fmt.Printf("video url: %s\tthumbnail path: %s\n", data.VideoUrl, path)
-			}
-			fmt.Printf("response:\nsuccessfully:%d\tfailed:%d\n", res.Total-res.Failed, res.Failed)
-			fmt.Println()
-		case localAsync:
-			wg := sync.WaitGroup{}
-			wg.Add(len(videoURLs))
-			chData := make(chan *proto.GetThumbnailRes, len(videoURLs))
-
-			for _, url := range videoURLs {
-				go func() {
-					defer wg.Done()
-
-					res, err := client.GetThumbnails(context.Background(), &proto.GetThumbnailReq{
-						VideoUrls: []string{url},
-						Async:     false,
-					})
-					if err == nil {
-						chData <- res
-					}
-				}()
-			}
-
-			go func() {
-				wg.Wait()
-				close(chData)
-			}()
-
-			succeeded := 0
-			for data := range chData {
-				if data.Failed != 0 && len(data.Videos) == 1 {
-					path, err := saveTo(context.Background(), strings.Split(data.Videos[0].VideoUrl, "?v=")[1], data.Videos[0].Thumbnail)
-					if err != nil {
-						fmt.Printf("failed to save thumbnail: %s\n", err.Error())
-						continue
-					}
-
-					fmt.Printf("video url: %s\tthumbnail path: %s\n", data.Videos[0].VideoUrl, path)
-					succeeded++
-				}
-			}
-			fmt.Printf("response:\nsuccessfully:%d\tfailed:%d\n", succeeded, len(videoURLs)-succeeded)
-			fmt.Println()
-		default:
-			res, err := client.GetThumbnails(context.Background(), &proto.GetThumbnailReq{
-				VideoUrls: strings.Split(val, " "),
+				VideoUrls: []string{url},
 				Async:     false,
 			})
 			if err != nil {
 				fmt.Printf("failed to get thumbnails: %s\n", err.Error())
+				return
+			}
+
+			chData <- res
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(chData)
+	}()
+
+	succeeded := 0
+	for data := range chData {
+		if data.Failed == 0 && len(data.Videos) == 1 {
+			path, err := saveTo(context.Background(), strings.Split(data.Videos[0].VideoUrl, "?v=")[1], data.Videos[0].Thumbnail)
+			if err != nil {
+				fmt.Printf("failed to save thumbnail: %s\n", err.Error())
 				continue
 			}
 
-			for _, data := range res.Videos {
-				path, err := saveTo(context.Background(), strings.Split(data.VideoUrl, "?v=")[1], data.Thumbnail)
-				if err != nil {
-					res.Failed++
-					fmt.Printf("failed to save thumbnail: %s\n", err.Error())
-					continue
-				}
+			fmt.Printf("video url: %s\tthumbnail path: %s\n", data.Videos[0].VideoUrl, path)
+			succeeded++
 
-				fmt.Printf("video url: %s\tthumbnail path: %s\n", data.VideoUrl, path)
-			}
-			fmt.Printf("response:\nsuccessfully:%d\tfailed:%d\n", res.Total-res.Failed, res.Failed)
-			fmt.Println()
+			continue
 		}
 	}
+	fmt.Printf("successfully:%d\tfailed:%d\n", succeeded, len(videoURLs)-succeeded)
+	fmt.Println()
+}
+
+func defaultSync(client proto.PreviewProxyClient, videoURLS []string) {
+	res, err := client.GetThumbnails(context.Background(), &proto.GetThumbnailReq{
+		VideoUrls: videoURLS,
+		Async:     false,
+	})
+	if err != nil {
+		fmt.Printf("failed to get thumbnails: %s\n", err.Error())
+		return
+	}
+
+	for _, data := range res.Videos {
+		path, err := saveTo(context.Background(), strings.Split(data.VideoUrl, "?v=")[1], data.Thumbnail)
+		if err != nil {
+			res.Failed++
+			fmt.Printf("failed to save thumbnail: %s\n", err.Error())
+			continue
+		}
+
+		fmt.Printf("video url: %s\tthumbnail path: %s\n", data.VideoUrl, path)
+	}
+	fmt.Printf("successfully:%d\tfailed:%d\n", res.Total-res.Failed, res.Failed)
+	fmt.Println()
 }
 
 const filesDir = "files"
